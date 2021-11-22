b0d5ce89bc6f4e4b86880275e0be5a41e7241dde
math-chacha <grandtail@naver.com>
2021-11-22 17:00:28 +0900

    PART3통계분석_2장_T-검정 작성 완료

--- R/PART3통계분석_1장_데이터 샘플링.r
+++ R/PART3통계분석_1장_데이터 샘플링.r
@@ -1,11 +1,7 @@
+# 윤종식, 『ADP 실기 데이터 분석 전문가』, (주)데이터에듀-2020, 136-142
+
 # 데이터 샘플링
 
-
-## 용어 정리
-### 모집단(population) : 조사하고자하는 대상 집단 전체
-### 원소(element) : 모집단을 구성하는 개체
-### 표본(sample) : 조사하기 위해 추출한 모집단의 일부 원소
-### 모수(parameter) : 표본 관측에 의해 구하고자 하는 모집단에 대한 정보
 
 ## 표본추출 방법
 ## 1. 단순 임의 추출법(simple random sampling)
@@ -18,27 +14,26 @@
 ##    모집단이 이질적인 몇 개의 계층으로 이루어져 있을 때 모든 계층으로부터 원소를 임의로 추출하여 각 계층을 고루 대표할 수 있도록 랜덤하게 표본을 추출하는 방법
 ## 5. 다단계 추출(multi-stage sampling)
 ##    표본 추출 과정을 여러 단계로 나누어서 진행하는 것으로, 표본으로 추출된 집단 내에서 다시 일부를 뽑고 그 집단에서 다시 일부를 뽑는 방법
-##    ex. 우리나라 시도를 무작위 선택 후 선택된 시도에서 군과 구를 무작위 선정, 다시 읍면동 선정한 후 해당 읍면동에 속한 주민등록번호를 무작위로 선택해 표본 추출
 
 ## R을 이용한 표본 추출
 ## 1.단순 임의 추출
 ##   sample(x, size, replace=FALSE, prop=NULL)  x : 표본 추출 벡터 / size : 표본 크기 / replace : 복원추출여부 / prob : 데이터 뽑을 때 가중치 지정
-##   example (iris 데이터 7:3 추출)
-train_idx <- sample(c(1:nrow(iris)), size = 0.7 * nrow(iris), replace=FALSE) 
+##   example (iris 데이터 6:4 추출)
+train_idx <- sample(c(1:nrow(iris)), size = 0.6 * nrow(iris), replace=FALSE) 
 train_df <- iris[train_idx,]
 test_df <- iris[-train_idx,]
-cat(nrow(iris),nrow(train_df),nrow(test_df)) # 150, 105, 45
+cat(nrow(iris),nrow(train_df),nrow(test_df)) # 150, 90, 60
 
 ## 2. 층화 임의 추출
 ##    sampling::strata(data, stratanames=NULL,size, method = c("srswor","srswr","poisson","systematic"), pik, description=FALSE)
 ##    stratanames : 계층 구분 변수 / method : 차례로 비복원단순임의추출, 복원단순임의추출, 포아송추출, 계통추출 / pik : 데이터를 표본에 포함시킬 확률 / description : 표본크기와 모집단 크기 추출할지 여부
 ##    getdata(data,m) m : 추출된 벡터 혹은 데이터 프레임
 ##    strata 함수 통해 층화 임의 추출한 데이터는 getdata함수 통해 확인 가능
-##    example(iris에서 Species별 20,15,15 층화임의추출)
+##    example (iris에서 Species별 10,5,5 층화임의추출)
 library(sampling)
-sample <- sampling::strata(data = iris, stratanames=c("Species"), size = c(20,15,15), method="srswor")
+sample <- sampling::strata(data = iris, stratanames=c("Species"), size = c(10,5,5), method="srswor")
 iris_sample <- getdata(iris, sample)
-table(iris_sample$Species) # setosa : 20 / versicolor : 15 / virginica : 15
+table(iris_sample$Species) # setosa : 10 / versicolor : 5 / virginica : 5
 
 
 
---
+++ R/PART3통계분석_2장_T-검정.r
@@ -0,0 +1,59 @@
+# 윤종식, 『ADP 실기 데이터 분석 전문가』, (주)데이터에듀-2020, 136-142
+
+
+# T-검정
+
+
+## 1. 일표본 T-검정
+##    단일모집단에서 관심 있는 연속형 변수의 평균값을 특정 기준값과 비교할 때 사용하는 검정방법
+##    가정 : 모집단 원소들의 정규성(shapiro.test 활용)
+##    가설 설정
+##    H_0 : 모평균의 값은 OO다.
+##    H_1 : 모평균의 값은 OO이 아니다.(양측)
+##    H_1 : 모평균의 값은 OO보다 작다.(좌단측)
+##    H_1 : 모평균의 값은 OO보다 크다.(우단측)
+
+##    R code
+##    1) 정규성 검정
+##       shapiro.test(data)   기각 시 정규분포 만족 X
+
+##    2) T검정
+##       정규분포 만족 -> t.test(x, alternative=c("two.sided","less","greater"), mu=0)
+##       정규분포 불만족 -> wilcox.test(x, alternative=c("two.sided","less","greater"), mu=0)
+##    example (iris Sepal.Length 평균이 5.5인지)
+shapiro.test(iris$Sepal.Length) # p-value : 0.01018 -> 정규분포 불만족
+wilcox.test(iris$Sepal.Length, alternative="two.sided",mu=5.5) # p-value : 9.105e-06 -> 평균 5.5라는 귀무가설 기각!
+
+
+## 2. 대응표본 T-검정
+##    단일모집단에 대해 두 번의 처리 가했을 때, 두 처리에 따른 평균 차이 비교
+##    가정 : 모집단 원소들의 정규성
+##    가설 설정
+##    H_0 : 두 모평균의 차이는 0이다.
+
+##    R code
+##    1) 대응표본 T검정
+##    t.test(x, y, alternative=c("two.sided","less","greater"), paired=FALSE, m=0)
+##    example (before/after 좌단측 대응표본 T-검정)
+df <- data.frame(before = c(2,1,3,4,5,2,1),
+                 after = c(3,1,5,5,5,4,5))
+t.test(df$before, df$after, alternative="less",paired=TRUE) # p-vale : 0.01767 -> before/after 평균 차이가 없다는 귀무가설 기각!(==before -> after 값이 커졌다)
+
+
+## 3. 독립표본 T-검정
+##    두 독립된 모집단의 평균 비교
+##    가정 : 두 모집단의 정규성 / 서로 독립 / 등분산성 가정 확인필요
+##    가설 설정
+##    H_0 : 두 모평균의 차이는 0이다.
+
+##    R code
+##    1) 등분산성 검정
+##       var.test(x, y, data, alternative)   / var.test(formula, data, alternative)
+
+##    2) 독립표본 T검정
+##       t.test(x,y, alternative, var.equal=FALSE) / t.test(formula, data, alternative, var.equal=FALSE)
+
+##    example (iris Sepal.Length/Sepal.Width 독립표본 검정)
+var.test(iris$Sepal.Length, iris$Sepal.Width) # p-value : 3.597e-14 -> 등분산 기각! 이분산성
+t.test(iris$Sepal.Length, iris$Sepal.Width, alternative="two.sided",val.equal=FALSE) # p-value : 2.2e-16 -> 귀무가설 기각! 두 모집단 평균 차이가 있다
+
